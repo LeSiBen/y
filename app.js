e1ab463afdd159124157fd6c8d59106b6c2ac8f7
LeSiBen <benwinner71@gmail.com>
2025-03-11 08:26:05 +0700

    fuck

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
-var controller = require(__dirname  + "/controllers");
-app.use(controller);
-var server = app.listen(3000, function(){
-   console.log("server is running");
+var controller = require(__dirname + "/controllers"); 
+var homecontroller   = require(__dirname + "/controllers/homecontroller.js"); 
+app.use("/", homecontroller); 
+
+var server = app.listen(3000, function() {
+    console.log("Server is running");
 });
 
-app.set("views",__dirname + "/views");
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use("/static", express.static(__dirname + "/public"));
